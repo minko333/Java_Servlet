d0d177f9dd27e3d30d53ae4b05d9d9941c08654a
Min Ko Ko <mko42776@gmail.com>
2026-04-21 07:00:02 +0630

    testing SessionMethods

--- src/main/java/com/example/demo/SessionServlet.java
+++ src/main/java/com/example/demo/SessionServlet.java
@@ -6,11 +6,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import java.io.IOException;
+
 @WebServlet("/Session")
 public class SessionServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response){
         //Getting the session object
         HttpSession session = request.getSession();
+
+        //setting the session attribute
+        session.setAttribute("userName","Minkoko");
+
+        //getting session attribute
+        String userName = (String) session.getAttribute("userName");
+
+        try {
+            // Display the attribute value in the response
+            response.getWriter().println("Username from Session: " + userName);
+
+            // Invalidate the session after displaying the attribute
+            session.invalidate();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
